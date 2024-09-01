b51ffd7957f2c86bd7d1f5f31fb832092f9273e0
samwithbeard <65282597+samwithbeard@users.noreply.github.com>
2024-09-01 13:51:36 +0200

    Update telliracer.py
    version 1.0 wich was used during the performace. 31.08.2024

--- telliracer.py
+++ telliracer.py
@@ -6,7 +6,7 @@ import time
 
 # Initialize player positions
 
-stepSize=1
+stepSize=100/21#% increase per pulse
 scores = []
 print("------------------------------------------")
 print("------------- telli racer ----------------")
@@ -46,7 +46,7 @@ broker_address = "192.168.0.101"  # Use your broker address 5672
 player1.topic = "race/player1"  # Topic for player 1
 player2.topic = "race/player2"  # Topic for player 2
 
-MQTT_BROKER="192.168.0.101"  # Use your broker address 5672
+MQTT_BROKER=broker_address  # Use your broker address 5672
 MQTT_PORT = 1883  # Default port for MQTT
 
 MQTT_TOPIC_STATE = "race/gamestate"
@@ -76,16 +76,21 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, message):
+    global GAME_STATE
+    player=0
     print(f"Received message: '{message.payload.decode()}' on topic '{message.topic}'")
-    try:
-        if message.topic == player1.topic:
+    try:        
+        if message.topic == player1.topic and GAME_STATE == "race started":
             player1.position += stepSize
+            player=1
             print(f"Player 1 is at {player1.position}%")
-        elif message.topic == player2.topic:
+        elif message.topic == player2.topic and GAME_STATE == "race started":
             player2.position += stepSize
+            player=2
             print(f"Player 2 is at {player2.position}%")
         elif message.topic == MQTT_TOPIC_DATA:
             message.payload.decode()
+            player=0
             #print(f"race data {message.payload.decode()}%")
             if message.payload.decode()[:4]=="BUZZ":
                 print("BUZZER RECEIVED")
@@ -95,8 +100,12 @@ def on_message(client, userdata, message):
     except:
         print("message reception failed")
     
-    message = str(player1.position)+";"+str(player2.position)
-    client.publish(MQTT_TOPIC_POSITION, message)
+    message = str(player1.position)+";"+str(player2.position)+";"+str(player)
+    if GAME_STATE == "race started": 
+        client.publish(MQTT_TOPIC_POSITION, message)
+        print("position report "+GAME_STATE)
+    else:
+        print("position report suppressed "+GAME_STATE)
 
 # Create an MQTT client instance
 client = mqtt.Client()
@@ -124,12 +133,14 @@ def clear_event_queue():
         
 # Race function
 def run_race():
+    global GAME_STATE
     clear_event_queue()
     buzz =False
     player1.position=0
     player2.position=0    
     
-    message = "race wait to start"
+    GAME_STATE = "race wait to start"
+    message = GAME_STATE
     print("-----state:"+ message)
     clear_event_queue()
     print("wait for button to be pressed..")
@@ -138,23 +149,26 @@ def run_race():
         if not event_queue.empty():
                 event = event_queue.get()
                 if event == "buzz":
-                    buzz = True 
-                    message=print("Buzz start")
+                    buzz = True                     
+                    GAME_STATE = "Buzz start"
+                    message = GAME_STATE
                     client.publish(MQTT_TOPIC_STATE, message)                                     
                     print(message)
-    buzz =False    
-    message = "countdown"
+    buzz =False
+    GAME_STATE = "countdown"
+    message = GAME_STATE   
     print("-----state:"+ message)
     client.publish(MQTT_TOPIC_STATE, message)
     
     print("The race starts in:")
-    for i in range(3, 0, -1):
+    for i in range(6, 0, -1):
         print(i)
         time.sleep(1)
     print("Go!") 
     player1.position=0
-    player2.position=0       
-    message = "race started"
+    player2.position=0          
+    GAME_STATE = "race started"
+    message = GAME_STATE 
     print("-----state:"+ message)
     client.publish(MQTT_TOPIC_STATE, message)
     clear_event_queue()
@@ -179,18 +193,24 @@ def run_race():
         if not event_queue.empty():
             event = event_queue.get()
             if event == "buzz":
-                buzz = True                    
-                message=print("Buzz cancel")
+                buzz = True                   
+                
+                GAME_STATE = "Buzz cancel"
+                message = GAME_STATE 
+                print("-----state:"+ message)
                 client.publish(MQTT_TOPIC_STATE, message)                                     
-                print(message)
+              
         time.sleep(1)
     print("loopended")
     # Stop the stopwatch
     end_time = time.time()
     race_time = end_time - start_time
-    message = "race ended"
+
+    GAME_STATE = "race ended"
+    message = GAME_STATE 
     print("-----state:"+ message)
-    client.publish(MQTT_TOPIC_STATE, message)
+    client.publish(MQTT_TOPIC_STATE, message) 
+
     time.sleep(1)
     if player1.position >= 100 and player2.position >= 100:
         print("It's a tie!")
@@ -200,27 +220,32 @@ def run_race():
         scores.append({'player': player2.name, 'time': race_time})
         winner="both"
         message="winner both"
-        print("-----state:"+ message)    
+        GAME_STATE = message 
+        print("-----state:"+ message)  
         client.publish(MQTT_TOPIC_STATE, message) 
     elif player1.position >= 100:
         print("Player 1 wins!")
         winner= 'Player 1'
         scores.append({'player': player1.name, 'time': race_time})
         message="winner 1"
-        print("-----state:"+ message)    
-        client.publish(MQTT_TOPIC_STATE, message) 
+        GAME_STATE = message 
+        print("-----state:"+ message)  
+        client.publish(MQTT_TOPIC_STATE, message)    
     elif player2.position >= 100:
         print("Player 2 wins!")
         winner= 'Player 2'
         scores.append({'player': player2.name, 'time': race_time})
         message="winner 2"
-        print("-----state:"+ message)    
+        print("-----state:"+ message)  
+        GAME_STATE = message 
+        print("-----state:"+ message)  
         client.publish(MQTT_TOPIC_STATE, message) 
     else:
         print("no winner !")
         winner= 'none'
         message="winner none"
-        print("-----state:"+ message)    
+        GAME_STATE = message 
+        print("-----state:"+ message)  
         client.publish(MQTT_TOPIC_STATE, message)  
         
     
@@ -231,76 +256,12 @@ def run_race():
     print(f"Race time: {race_time:.2f} seconds")
     player1.position=0
     player2.position=0
-    celebration_time=30#s
+    celebration_time=5#s
     print(str(celebration_time)+"celebration time")
     time.sleep(celebration_time) #celebration time
     # Clean up MQTT client
 
 
-class StateMachine:       
-    def __init__(self):
-        self.state = "Idle"
-
-    def transition(self, event):
-        if self.state == "Idle":
-            if event == "start":
-                self.state = "Countdown"
-                #state_countdown()
-            elif event == "next":
-                self.state = "Countdown"
-                #state_countdown()
-            else:
-                print("transition not available")
-        elif self.state == "Countdown":
-            if event == "next":
-                self.state = "Race"
-                #state_race()
-            elif event == "cancel":
-                self.state = "Idle"
-                #state_idle()
-            else:
-                print("transition not available")
-        elif self.state == "Race":
-            if event == "next":
-                self.state = "Winnercelebration"
-                #state_winner_cel()
-            elif event == "cancel":
-                self.state = "Idle"
-                #state_idle()
-            else:
-                print("transition not available")
-                
-        elif self.state == "Winnercelebration":
-            if event == "next":
-                self.state = "Idle"
-                #state_idle()
-            elif event == "cancel":
-                self.state = "Idle"
-                #state_idle()
-            elif event == "Record":
-                self.state = "Recordcelebration"
-                #state_record_celebration()
-            else:
-                print("transition not available")
-        elif self.state == "Recordcelebration":
-            if event == "next":
-                self.state = "Idle"
-                #state_idle()
-            elif event == "cancel":
-                self.state = "Idle"
-                #state_idle()
-            else:
-                print("transition not available")
-        
-        self.print_state()
-
-    def print_state(self):
-        print(f"Current state: {self.state}")
-
-
-# Example usage
-sm = StateMachine()
-sm.print_state()  # Initial state: Idle
 
 
 
